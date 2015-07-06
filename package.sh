ab9d74102a380f7ebeda0798963ae92bfde5984e
Jeff Hubbard <lord2800@gmail.com>
2015-07-06 01:18:29 -0700

    Use the pc1 version of puppet and hardcode the release name since there is no vivid release of puppet

--- package.sh
+++ package.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 VERSION="${1:-1.0}"
 
-fpm -s dir -t deb -v $VERSION -n puppet-firstboot --after-install post-install.sh --prefix /etc/init.d firstboot
+fpm -s dir -t deb -v $VERSION -n puppet-firstboot --after-install post-install.sh -C build/ .
